3ad167ef0ab10e78842c21d7e7d11f5a6d653128
Victor <vicopepin.oliveira@gmail.com>
2025-12-31 12:28:25 +0100

    Fix router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { CrosswordProvider } from './context/CrosswordContext';
-import { CrosswordEditor } from './pages/CrosswordEditor';
-import { Login } from './pages/Login';
-import { MyGrids } from './pages/MyGrids';
-import './styles/crossword.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { CrosswordProvider } from "./context/CrosswordContext";
+import { CrosswordEditor } from "./pages/CrosswordEditor";
+import { Login } from "./pages/Login";
+import { MyGrids } from "./pages/MyGrids";
+import "./styles/crossword.css";
 
 const App: React.FC = () => {
-    return (
-        <CrosswordProvider>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<CrosswordEditor />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/my-grids" element={<MyGrids />} />
-                </Routes>
-            </Router>
-        </CrosswordProvider>
-    );
+  return (
+    <CrosswordProvider>
+      <Router basename="/crossword-maker">
+        <Routes>
+          <Route path="/" element={<CrosswordEditor />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/my-grids" element={<MyGrids />} />
+        </Routes>
+      </Router>
+    </CrosswordProvider>
+  );
 };
 
 export default App;
